5ef48e820b5daad2f6f734fabc8811430bb076ab
vidishasrivastava142-maker <vidishasrivastava142@gmail.com>
2026-01-12 22:12:18 +0530

    AverageOfThree

---
+++ Basicprogram/AverageOfThree.java
@@ -0,0 +1,10 @@
+public class AverageOfThree {
+    public static void main(String[] args) {
+        double a = 6;
+        double b = 6.6;
+        double c = 7;
+
+        double avg = (a + b + c) / 3;
+        System.out.println("Average = " + avg);
+    }
+}
