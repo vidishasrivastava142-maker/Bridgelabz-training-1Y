59e1bf9305ac4bbe3f59bff783c435e421c1bedc
vidishasrivastava142-maker <vidishasrivastava142@gmail.com>
2026-01-12 22:27:35 +0530

    KmToMiles

---
+++ Basicprogram/KmToMiles.java
@@ -0,0 +1,7 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+        double km = 10.2;
+        double miles = km * 0.621371;
+        System.out.println("Miles = " + miles);
+    }
+}
