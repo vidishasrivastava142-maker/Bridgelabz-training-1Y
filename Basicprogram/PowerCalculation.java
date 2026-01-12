8829742090fcd358037c715c6dc8eaed2c003339
vidishasrivastava142-maker <vidishasrivastava142@gmail.com>
2026-01-12 16:57:26 +0530

    PowerCalculation

---
+++ Basicprogram/PowerCalculation.java
@@ -0,0 +1,9 @@
+public class PowerCalculation {
+    public static void main(String[] args) {
+        double base = 5;
+        double exponent = 2;
+
+        double result = Math.pow(base, exponent);
+        System.out.println("Result = " + result);
+    }
+}
