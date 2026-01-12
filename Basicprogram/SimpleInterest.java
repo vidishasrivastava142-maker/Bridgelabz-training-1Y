3801a4ec816c7be24083361cd467fa2e0ef4f5fe
vidishasrivastava142-maker <vidishasrivastava142@gmail.com>
2026-01-12 16:43:49 +0530

    SimpleInterest

--- Basicprogram/SimpleInterest.java
+++ Basicprogram/SimpleInterest.java
@@ -1,8 +1,8 @@
 public class SimpleInterest {
     public static void main(String[] args) {
-        double p = 50;
-        double r = 4;
-        double t = 2;
+        double p = 5;
+        double r = 2;
+        double t = 4;
 
         double si = (p * r * t) / 100;
         System.out.println("Simple Interest = " + si);
