0b0947eea0724c51bc4aa7c4c221aafee5b6137d
vidishasrivastava142-maker <vidishasrivastava142@gmail.com>
2026-01-12 16:52:36 +0530

    PerimeterRectangle

---
+++ Basicprogram/PerimeterRectangle.java
@@ -0,0 +1,9 @@
+public class PerimeterRectangle {
+    public static void main(String[] args) {
+        double length = 44;
+        double width = 20;
+
+        double perimeter = 2 * (length + width);
+        System.out.println("Perimeter = " + perimeter);
+    }
+}
