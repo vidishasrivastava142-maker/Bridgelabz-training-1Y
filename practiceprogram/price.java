93584829c7a5098b7ef9578b2698e15c53cea33a
vidishasrivastava142-maker <vidishasrivastava142@gmail.com>
2026-01-14 02:30:02 +0530

    price

---
+++ practiceprogram/price.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class price{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double unitPrice = sc.nextDouble();
+        int quantity = sc.nextInt();
+
+        double total = unitPrice * quantity;
+
+        System.out.println("The total purchase price is INR " + total +
+                " if the quantity " + quantity +
+                " and unit price is INR " + unitPrice);
+    }
+}
