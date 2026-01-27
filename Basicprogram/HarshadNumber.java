3234cded5d3dc835721db4fb7c20effd61965025
vidishasrivastava142-maker <vidishasrivastava142@gmail.com>
2026-01-27 22:03:57 +0530

    saved calculator changes

---
+++ Basicprogram/HarshadNumber.java
@@ -0,0 +1,27 @@
+ import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int originalNumber = number;
+        int sum = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+        }
+
+        if (originalNumber % sum == 0) {
+            System.out.println(originalNumber + " is a Harshad Number");
+        } else {
+            System.out.println(originalNumber + " is not a Harshad Number");
+        }
+
+    }
+}
